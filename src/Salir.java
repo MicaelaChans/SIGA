2c76d398813dc954e39abc3c0a63e3dcf084b3e8
Pablo Avila <pabloavila0716@gmail.com>
2024-10-21 10:53:33 -0300

    Cambios en el menu Salir
    Hice un pequeño cambio en el menú salir, todavía queda seguir modificando algunos detalles

--- src/Salir.java
+++ src/Salir.java
@@ -4,13 +4,16 @@ public class Salir {
 
 	public static void MostrarSalir() {
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("El Programa se ha Cerrado");
+		System.out.println("Usted desea Salir del Programa?");
+		System.out.println("1-Si/0-No");
+		int salir = teclado.nextInt();
+		
+		if (salir ==1) {
+			System.out.println("El Programa se ha Cerrado");
+		}else if(salir ==0) {
+			System.out.println("Usted volvio al Menú Principal");
 			
-		
-		
-		
-		teclado.close();
+		}teclado.close();
 		
 	}
-
 }
